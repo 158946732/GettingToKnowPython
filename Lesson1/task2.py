dcbabb3619f57d1bd31afb8b038de8b1bc42dea6
Alina <kalinka0611@mail.ru>
2023-02-21 23:25:14 +0300

    task2 correct

--- Lesson1/task2.py
+++ Lesson1/task2.py
@@ -1,13 +1,10 @@
 #Найдите сумму цифр трехзначного числа.
-#*Пример:*
-# 123 -> 6 (1 + 2 + 3)
-# 100 -> 1 (1 + 0 + 0)
 
 number = int(input("Введите трехзначное число: "))
-if (number < 100 or number > 999):
-    print("Вы ввели неверное число!")
 sum = 0
-while number > 0:
-    sum = sum + number % 10
-    number = number // 10
-print(f"Сумма цифр в числе равна: {sum}")
+if number > 99 and number < 1000:
+    while number > 0:
+        sum = sum + number % 10
+        number = number // 10
+    print(f"Сумма цифр в числе равна: {sum}")    
+else: print("Вы ввели неверное число!")
